6f7022c0c6b8e68b044303fa26e6b418d6542fb8
AravinthC-18 <96327761+AravinthC-18@users.noreply.github.com>
2022-05-11 14:48:54 +0530

    updating UI register

--- app/src/main/java/com/example/wm/Login_Register/RegisterActivity.java
+++ app/src/main/java/com/example/wm/Login_Register/RegisterActivity.java
@@ -2,17 +2,25 @@ package com.example.wm.Login_Register;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wm.MainActivity;
@@ -20,12 +28,19 @@ import com.example.wm.R;
 import com.example.wm.WebService_Class;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText name;
     private AutoCompleteTextView pin,cpin,phonenum;
     private String  s_name,s_phonenum,s_pin,s_cpin,TAG="RegisterActivity";
     private AppCompatButton register;
+    private TextView heads,about;
+    private ConstraintLayout top_bg;
+    private Animation slide_down_anim,slide_up_anim,fade_in_anim;
+    private LinearLayout linearLayout,registerbtn_layout;
     //private TextInputLayout name_layout,phonenum_layout,pin_layout,cpin_layout;
 
 
@@ -35,6 +50,18 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         name=findViewById(R.id.name);
+        heads=findViewById(R.id.heads);
+        about=findViewById(R.id.about);
+        top_bg=findViewById(R.id.head);
+        linearLayout=findViewById(R.id.linear);
+        registerbtn_layout=findViewById(R.id.registerbtn_layout);
+        //SetText(heads.getText().toString());
+        Top_Bg();
+        SetTextAbout("SIGN UP...",0);
+        SetTextAbout("Sign up here to get the most out of device.",1);
+
+
+
        /* name_layout=findViewById(R.id.name_layout);
         addTextChanger(name,name_layout);
         setOnFoucsChangeLister(name,name_layout);*/
@@ -75,13 +102,66 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
-  /*  public void hideKeyboard(View view) {
-        try {
-            InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        } catch(Exception ignored) {
-        }
-    }*/
+
+    private void Top_Bg() {
+        slide_down_anim = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_down);
+        slide_up_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+        fade_in_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        top_bg.startAnimation(slide_down_anim);
+        registerbtn_layout.startAnimation(slide_up_anim);
+        linearLayout.startAnimation(fade_in_anim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               /* Intent intent = new Intent(RegisterActivity.this, NextActivity.class);
+                startActivity(intent);*/
+
+            }
+        }, 5000);
+    }
+
+    private void SetTextAbout(String s,int n) {
+            final int[] i = new int[1];
+            i[0] = 0;
+            final int length = s.length();
+            final Handler handler = new Handler()
+            {
+                @SuppressLint("HandlerLeak")
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    char c= s.charAt(i[0]);
+                     if(n==0) {
+                        heads.append(String.valueOf(c));
+                    }
+                    else if(n==1) {
+                        about.append(String.valueOf(c));
+                    }
+                    i[0]++;
+                }
+            };
+            final Timer timer = new Timer();
+            TimerTask taskEverySplitSecond = new TimerTask() {
+                @Override
+                public void run() {
+                    handler.sendEmptyMessage(0);
+                    if (i[0] == length - 1) {
+                        timer.cancel();
+                    }
+                }
+            };
+            timer.schedule(taskEverySplitSecond, 1, 250);
+
+    }
+
+    /*  public void hideKeyboard(View view) {
+          try {
+              InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+              imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+          } catch(Exception ignored) {
+          }
+      }*/
     private void Getregisterdata() {
         s_cpin=cpin.getText().toString();
         s_pin=pin.getText().toString();
